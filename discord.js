8904d8692294264e74ccd79413ee1bc8ecb85f49
hp? <90970059+motionarium@users.noreply.github.com>
2021-12-03 14:07:52 +0300

    fixed js code.

--- discord.js
+++ discord.js
@@ -1,9 +1,8 @@
-function hide() { ds.style.display='none'; }
 async function copydiscord() {
     try {
         await navigator.clipboard.writeText(';w;#7576');
         ds.style.display='block';
-        setTimeout(hide, 2000);
+        setTimeout("ds.style.display='none'", 2000);
     }
     catch (err) {
         console.error('Ошибка: ', err);
