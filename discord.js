266e3515fea2503a65fcf0997318307e203971ae
hp? <90970059+motionarium@users.noreply.github.com>
2021-12-03 14:43:57 +0300

    mobile opera fix

--- discord.js
+++ discord.js
@@ -1,3 +1,18 @@
+if (navigator.clipboard) {
+    async function copydiscord() {
+        try {
+            await navigator.clipboard.writeText(';w;#7576');
+            ds.style.display='block';
+            setTimeout("ds.style.display='none'", 2000);
+        }
+        catch (err) {
+            console.error('Ошибка: ', err);
+        }
+    }
+} else {
+    ds1.style.display='block';
+}
+
 async function copydiscord() {
     try {
         await navigator.clipboard.writeText(';w;#7576');
