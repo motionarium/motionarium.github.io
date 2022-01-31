e28e6293f1bc310e88a8d6981f657669756a5b06
hp? <90970059+motionarium@users.noreply.github.com>
2022-01-31 10:55:05 +0300

    fixed delay

--- typed.js
+++ typed.js
@@ -2,13 +2,14 @@ document.addEventListener("DOMContentLoaded", function () {
 new TypeIt("#type", {
     speed: 125,
     loop: true,
-    nextStringDelay: 750,
     deleteSpeed: 150,
     cursor: false,
     })
     .type('Я <span class="name">motionarium</span>!')
+    .pause(750)
     .delete(".name")
     .type('hackerpepe!')
+    .pause(750)
     .go();
 })
 
@@ -16,13 +17,14 @@ document.addEventListener("DOMContentLoaded", function () {
     new TypeIt("#type2", {
         speed: 125,
         loop: true,
-        nextStringDelay: 750,
         deleteSpeed: 150,
         cursor: false,
         })
         .type('Страница не найдена!')
+        .pause(750)
         .delete()
         .type('o_O')
+        .pause(750)
         .go();
     })
 
@@ -30,13 +32,14 @@ document.addEventListener("DOMContentLoaded", function () {
     new TypeIt("#type3", {
         speed: 125,
         loop: true,
-        nextStringDelay: 750,
         deleteSpeed: 150,
         cursor: false,
         })
         .type('donate.')
+        .pause(750)
         .delete()
         .type('back.')
+        .pause(750)
         .go();
         })
         
