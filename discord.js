d3797a3ea73c71fbceb8fbe9fe08ed5e7b4c4206
hp? <90970059+motionarium@users.noreply.github.com>
2022-08-02 00:21:59 +0300

    fixed discord

--- discord.js
+++ discord.js
@@ -3,8 +3,6 @@ async function copydiscord() {
             await navigator.clipboard.writeText(';w;#7576');
             ds.style.display='block';
             setTimeout("ds.style.display='none'", 2000);
-            ds1.style.display='block';
-            setTimeout("ds1.style.display='none'", 2000);
         }
         catch (err) {
             console.error('Ошибка: ', err);
